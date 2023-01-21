e7a6674f9021a2ad16e755d67d23237920a24c1c
injinjeong <snowwwwww@naver.com>
2023-01-21 10:49:38 +0900

    ApplicationContext

--- src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
+++ src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
@@ -1,9 +1,11 @@
 package com.me.spring.tobyspring;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -20,26 +22,31 @@ public class TobySpringApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(TobySpringApplication.class, args);
+//        HelloController helloController = new HelloController();
 
-        HelloController helloController = new HelloController();
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능
+
                     String requestURL = req.getRequestURI();
                     String method = req.getMethod();
 
                     if (requestURL.equals("/hello") && method.equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                        resp.setStatus(HttpStatus.OK.value());
+//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
-                    } else if (requestURL.equals("/user")) {
-                        //...
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
