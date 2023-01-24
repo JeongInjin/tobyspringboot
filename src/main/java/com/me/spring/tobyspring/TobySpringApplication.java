b4fe3c5e3b0de3bb2f59270ad2b8f566b2a29c80
injinjeong <snowwwwww@naver.com>
2023-01-24 21:56:02 +0900

    Bean Factory

--- src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
+++ src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
@@ -1,65 +1,49 @@
 package com.me.spring.tobyspring;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
 public class TobySpringApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloServic() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
 //        SpringApplication.run(TobySpringApplication.class, args);
 //        HelloController helloController = new HelloController();
 
 //        GenericApplicationContext applicationContext = new GenericApplicationContext();
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this) {
+                    }).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+
+        applicationContext.register(TobySpringApplication.class);
         applicationContext.refresh();
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-//            servletContext.addServlet("frontcontroller", new HttpServlet() {
-//                @Override
-//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//                    // 인증, 보안, 다국어, 공통 기능
-//
-//                    String requestURL = req.getRequestURI();
-//                    String method = req.getMethod();
-//
-//                    if (requestURL.equals("/hello") && method.equals(HttpMethod.GET.name())) {
-//                        String name = req.getParameter("name");
-//
-//                        HelloController helloController = applicationContext.getBean(HelloController.class);
-//                        String ret = helloController.hello(name);
-////                        resp.setStatus(HttpStatus.OK.value());
-////                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-//                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-//                        resp.getWriter().println(ret);
-//                    } else {
-//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-//                    }
-//                }
-//            }).addMapping("/*");
-            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) {
-            }).addMapping("/*");
-        });
-        webServer.start();
     }
 
 }
