8d3a089591918123fcfc9ff400ea3e7966ca7f6c
injinjeong <snowwwwww@naver.com>
2023-01-24 21:34:54 +0900

    dispatcher Servlet

--- src/main/java/com/me/spring/tobyspring/HelloController.java
+++ src/main/java/com/me/spring/tobyspring/HelloController.java
@@ -1,11 +1,16 @@
 package com.me.spring.tobyspring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
 /**
  *  http -v ":8080/hello?name=jjj"
  */
 //@RestController
+    @RequestMapping
 public class HelloController {
 
 //    @GetMapping("/hello")
@@ -15,6 +20,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
+++ src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,34 +26,37 @@ public class TobySpringApplication {
 //        SpringApplication.run(TobySpringApplication.class, args);
 //        HelloController helloController = new HelloController();
 
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+//        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능
-
-                    String requestURL = req.getRequestURI();
-                    String method = req.getMethod();
-
-                    if (requestURL.equals("/hello") && method.equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-//                        resp.setStatus(HttpStatus.OK.value());
-//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
+//            servletContext.addServlet("frontcontroller", new HttpServlet() {
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    // 인증, 보안, 다국어, 공통 기능
+//
+//                    String requestURL = req.getRequestURI();
+//                    String method = req.getMethod();
+//
+//                    if (requestURL.equals("/hello") && method.equals(HttpMethod.GET.name())) {
+//                        String name = req.getParameter("name");
+//
+//                        HelloController helloController = applicationContext.getBean(HelloController.class);
+//                        String ret = helloController.hello(name);
+////                        resp.setStatus(HttpStatus.OK.value());
+////                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+//                        resp.getWriter().println(ret);
+//                    } else {
+//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+//                    }
+//                }
+//            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) {
             }).addMapping("/*");
         });
         webServer.start();
