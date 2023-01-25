b06b13d1fd332a9a101bf931ca5ea6e3ff430ff9
injinjeong <snowwwwww@naver.com>
2023-01-25 16:46:34 +0900

    springApplication

--- src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
+++ src/main/java/com/me/spring/tobyspring/TobySpringApplication.java
@@ -1,5 +1,6 @@
 package com.me.spring.tobyspring;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import static com.me.spring.tobyspring.MySpringApplication.run;
 
 @Configuration
 @ComponentScan
@@ -24,8 +24,7 @@ public class TobySpringApplication {
     }
 
     public static void main(String[] args) {
-        run(TobySpringApplication.class, args);
-
+        SpringApplication.run(TobySpringApplication.class, args);
     }
 
 }
