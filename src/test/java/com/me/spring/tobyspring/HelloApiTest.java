fd5df18d3dfbc6fde2d42fcadc283551297e4a8c
injinjeong <snowwwwww@naver.com>
2023-03-01 21:00:10 +0900

    java 17

--- src/test/java/com/me/spring/tobyspring/HelloApiTest.java
+++ src/test/java/com/me/spring/tobyspring/HelloApiTest.java
@@ -1,5 +1,6 @@
 package com.me.spring.tobyspring;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 class HelloApiTest {
 
     @Test
+    @Disabled
     void helloAPi() {
         TestRestTemplate rest = new TestRestTemplate();
         ResponseEntity<String> result = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Jeong");
